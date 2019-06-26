64b31c90417f23f190902d810514182e4cf1db4b
Jennifer Parker <jennifer.parker1@verizon.com>
2019-06-26 17:24:32 -0500

    daily commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,53 @@
 $('#name').focus();
 
+const $jsPunShirtColors =("cornflowerblue","darkslategrey","gold");
+const $heartJsShirtColors=("tomato","steelblue","dimgrey");
 
+$('#other-title').hide()
 
-$("#title"){
+$('#title').click(function(){
+       if ($('#title').val()==='other'){
+         $('#other-title').show();
+        
+    }
+       else $('#other-title').hide();
+
+      });
+
+ $('#design').click(function(){
+   if ($('#design').val ==="js puns"){
+     ($jsPunShirtColors).show();
     
-
-
 }
+   else $('#color').hide($heartJsShirtColors);
+
+  });
+
+
+
+$("#color")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+

1dd84421cade5de1b68f3208122c87e28cdeb9bb
Jennifer Parker <jennifer.parker1@verizon.com>
2019-07-03 15:47:47 -0500

    evening append

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
+var checkbox1 = $("input[name='all']" ) 
+var checkbox2 = $("input[name='js-frameworks']")
+var checkbox3 = $("input[name='js-libs']" )
+var checkbox4 = $("input[name='express']") 
+var checkbox5 = $("input[name='node']") 
+var checkbox6 = $("input[name='build-tools']") 
+var checkbox7 = $("input[name='npm']") 
 $('#name').focus();
+//const nameInput = document.getElementById("name");
+// const zipInput = document.getElementById("zip");
+// const cvvInput = document.getElementById("cvv");
+// const emailInput = document.getElementById("mail");
+// const creditCardInput = document.getElementById("cc-num");
+// const expMonth = document.getElementById("exp-month");
+// const expYear = document.getElementById("exp-year");
+$(document).ready(function(){
+  
+  
+  
 
-//const jsPunShirtColors =("cornflowerblue","darkslategrey","gold");
-//const heartJsShirtColors=("tomato","steelblue","dimgrey");
+
+
 
 $('#other-title').hide()
 
@@ -38,7 +56,7 @@ else if ($(this).val()==="heart js"){
 });
 //activies register
 
-('input[type="checkbox"]')
+
 
 //payment info
 
@@ -89,147 +107,117 @@ else {
 
 //}
 )
+
 //register for activities
-$("input[type='checkbox']").change(function(){
 
-var checkbox1 = $("input[name='all']" ) 
-var checkbox2 = $("input[name='js-frameworks']")
-var checkbox3 = $("input[name='js-libs']" )
-var checkbox4 = $("input[name='express']") 
-var checkbox5 = $("input[name='node']") 
-var checkbox6 = $("input[name='build-tools']") 
-var checkbox7 = $("input[name='npm']") 
-let $total=0
+  
+$('.activities').change(function(){
 
+// //let total = 0
+// let totalSum = 0;
+// let activities = $('.activities input[type = checkbox]');
 
-
-//total starts at 0
+// //total starts at 0
    
 //items in check box (checked or not)
 if (checkbox1.is(':checked')){
-    $total += 200;
+    
 }
 if (checkbox2.is(':checked')){
-    $total += 100;
+    
     checkbox4.prop('disabled',true);
 }else {
     checkbox4.prop('disabled',false);
 }
 
 if (checkbox3.is(':checked')){
-  $total += 100;
+  
   checkbox5.prop('disabled',true);
 }else {
  checkbox5.prop('disabled',false);
 }
 if (checkbox4.is(':checked')){
-  $total += 100;
+  
   checkbox2.prop('disabled',true);
 }else {
  checkbox2.prop('disabled',false);
 }
 if (checkbox5.is(':checked')){
-  $total += 100;
+  
   checkbox3.prop('disabled',true);
 }else {
  checkbox3.prop('disabled',false);
 }
 if (checkbox6.is(':checked')){
-  $total += 100;
-}
+  
+}else{}
 if (checkbox7.is(':checked')){
-  $total += 100;
-}
+  
+}else{}
+
+
+});
+
+var totalCost = 0;
+
+$('.activities').append('<div id="total"></div>');
+var updateCost = function (cost) {
+    totalCost += cost;
+    document.getElementById("total").innerHTML = "Total: $" + totalCost;
+};  
+$('input[name="all"]').change(function () {
+    if ($(this).prop("checked")) {
+        updateCost(200);
+    } else {
+        updateCost(-200);
+    }
+});
+$('input[name="js-frameworks"]').change(function () {
+    if ($(this).prop("checked")) {
+        updateCost(100);
+    } else {
+        updateCost(-100);
+    }
+});
+$('input[name="js-libs"]').change(function () {
+    if ($(this).prop("checked")) {
+        updateCost(100);
+    } else {
+        updateCost(-100);
+    }
+});
+$('input[name="express"]').change(function () {
+    if ($(this).prop("checked")) {
+        updateCost(100);
+    } else {
+        updateCost(-100);
+    }
+});
+$('input[name="node"]').change(function () {
+    if ($(this).prop("checked")) {
+        updateCost(100);
+    } else {
+        updateCost(-100);
+    }
+});
+$('input[name="build-tools"]').change(function () {
+    if ($(this).prop("checked")) {
+        updateCost(100);
+    } else {
+        updateCost(-100);
+    }
+});
+$('input[name="npm"]').change(function () {
+    if ($(this).prop("checked")) {
+        updateCost(100);
+    } else {
+        updateCost(-100);
+    }
 });
 
 
 
-//competative activities? disable
-    //enable when unchecked
-    //add amount for each enabled event
 
 
-    //total should reflect selected items
-    
-    //checkbox listener
 
-
-//necessary variables
-const nameInput = document.getElementById("name");
-const zipInput = document.getElementById("zip");
-const cvvInput = document.getElementById("cvv");
-const emailInput = document.getElementById("mail");
-const creditCardInput = document.getElementById("cc-num");
-const expMonth = document.getElementById("exp-month");
-const expYear = document.getElementById("exp-year");
-/**
- * 
- * VALIDATORS
- *  
- */
-
-//name Cant be blank
-function isValidName(name) {
-  return/\w+/.test(name)
-  
-  }
-  
-  // Must contain a lowercase, uppercase letter and a number
-  //function isValidPassword(password) {
-  
-    //return/[a-z]/.test(password) && 
-       //   /[A-Z]/.test(password) && 
-        //  /[0-9]/.test(password)
- // }
-  
-  // The telephone number must be in the format of (555) 555-5555
-  //function isValidTelephone(telephone) {
-    
-   // const regex=  /^\D*(\d{3})\D*(\d{3})\D*(\d{4})\D*$/.test(telephone);
-    //return text.replace(regex, ($1) $2-$3);
-  
- // }
-  
-  //Must be a valid email address
-  function isValidEmail(mail) {
-  
-  return /^[^@]+@[^@.]+\.[a-z]+$/i.test(mail);
-  }
-  
-  /**
-   * 
-   * FORMATTING FUNCTIONS
-   * 
-   */
-  
-  
-  
-  function showOrHideTip(show, element) {
-   // show element when show is true, hide when false
-  if (show) {
-      element.style.display = "inherit";
-    } else {
-     element.style.display = "none";
-    }
-  }
-  
-  function createListener(validator) {
-    return e => {
-      const text = e.target.value;
-      const valid = validator(text);
-      const showTip = text !== "" && !valid;
-      const tooltip = e.target.nextElementSibling;
-      showOrHideTip(showTip, tooltip);
-    };
-  }
-  
-  nameInput.addEventListener("input", createListener(isValidName));
-  
-  //passwordInput.addEventListener("input", createListener(isValidPassword));
-  
-  //telephoneInput.addEventListener("input", createListener(isValidTelephone));
-  
-  emailInput.addEventListener("input", createListener(isValidEmail));
-  
-
-
+})
